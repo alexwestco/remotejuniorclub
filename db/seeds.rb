d9a1b39fc1a53991060fabbedce32dec302a50cd
alexandersideris <alexander.sideris.west@gmail.com>
2018-03-30 16:11:45 +0300

    Built web scraper

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 require 'open-uri'
 require 'nokogiri'
 
-
 $jobs = Array.new([])
 
 def createJob(title, company, url)
@@ -11,12 +10,10 @@ def createJob(title, company, url)
 
 	puts url
 
-	# Check if the job already exists in the database. We will pretend that names combined with companies are unique
-
 	j = Job.where(title: title).take
 
 	if j == nil
-		j = Job.create(title: title, company: company, url: url)
+		j = Job.create(title: title, url: url)
 	else
 		j.title = title
 		j.company = company
@@ -35,12 +32,37 @@ content = document.read
 
 parsed_content = Nokogiri::HTML(content)
 
-puts parsed_content
-
 # Get the divisions
-posts = parsed_content.css('div#fighter-rankings').css('div#ranking-lists').css('.ranking-list')
+posts = parsed_content.css('div#timeline').css('.tweet').css('.content').css('.card2').css('.js-macaw-cards-iframe-container')
 
-divisions.each{|division|
+puts posts.length
+
+
+posts.each{|post|
+
+	puts 'LINK: ' + post['data-card-url']
+	puts ''
+	puts 'extra info link: https://twitter.com' + post['data-full-card-iframe-url']
+	puts ''
+
+	document_two = open('https://twitter.com'+post['data-full-card-iframe-url'])
+	content_two = document_two.read
+
+	parsed_content_two = Nokogiri::HTML(content_two)
 	
+	title = "TITLE: " + parsed_content_two.css('.SummaryCard-content').css('.TwitterCard-title').inner_html
+
+	puts title
+	puts ''
+
+	desc = "DESCRIPTION: " + parsed_content_two.css('.SummaryCard-content').css('.tcu-resetMargin').inner_html
+
+	puts desc
+	puts ''
+
+	puts ''
+
 
 }
+
+
