c2aab38bdecda235e2604c1945e88102d6e416a8
alexandersideris <alexander.sideris.west@gmail.com>
2018-04-10 12:27:11 +0300

    Fixed front page design somewhat

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@ class WelcomeController < ApplicationController
   # GET /welcome
   def index
   	@developers = Developer.all
+  	@side_projects = SideProject.all
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,12 +21,9 @@ def createJob(title, description, url)
 
 end
 
-puts "---------POSTS---------"
 
-Job.destroy_all
-websites = ['https://twitter.com/authenticjobs','https://twitter.com/StackDevJobs', 'https://twitter.com/dribbblejobs', 'https://twitter.com/hasjob', 'https://twitter.com/jobmote', 'https://twitter.com/jobmote', 'https://twitter.com/Jobspresso', 'https://twitter.com/nofluffjobs', 'https://twitter.com/remote_ok', 'https://twitter.com/wfhio' ,'https://twitter.com/workingnomads', 'https://twitter.com/weworkremotely']
+def scrape(website)
 
-websites.each{|website|
 	document = open(website)
 	content = document.read
 
@@ -34,11 +31,13 @@ websites.each{|website|
 
 	# Get the divisions
 	posts = parsed_content.css('div#timeline').css('.tweet').css('.content').css('.card2').css('.js-macaw-cards-iframe-container')
+	tweet_texts = parsed_content.css('div#timeline').css('.tweet').css('.content').css('.js-tweet-text-container').css('.tweet-text')
 
-
+	i = 0
 	posts.each{|post|
 
-		
+		tweet_text = tweet_texts[i].inner_html
+
 		url = post['data-card-url']
 
 		document_two = open('https://twitter.com'+post['data-full-card-iframe-url'])
@@ -51,10 +50,108 @@ websites.each{|website|
 
 		description = "DESCRIPTION: " + parsed_content_two.css('.SummaryCard-content').css('.tcu-resetMargin').inner_html
 
+		if website == 'https://twitter.com/authenticjobs'
 
-		createJob(title, description, url)
+			if tweet_text.include? "Junior"
+				if tweet_text.include? "<s>#</s><b>remote</b>"
+					createJob(title, description, url)
+				end
+			end
+			
+
+		elsif website == 'https://twitter.com/StackDevJobs'
+
+			if tweet_text.include? "Junior"
+
+				if tweet_text.include? "(Remote)"
+					createJob(title, description, url)
+					break
+				end
+
+				if tweet_text.include? "[Remote]"
+					createJob(title, description, url)
+					break
+				end
+				
+			end
+
+		elsif website == 'https://twitter.com/dribbblejobs'
+
+			if tweet_text.include? "Junior"
+
+				if tweet_text.include? "anywhere"
+					createJob(title, description, url)
+					break
+				end
+
+			end
+
+		elsif website == 'https://twitter.com/hasjob'
+
+			if tweet_text.include? "Junior"
+
+				if tweet_text.include? "<s>#</s><b>Anywhere</b>"
+					createJob(title, description, url)
+					break
+				end
+
+			end
+
+		elsif website == 'https://twitter.com/Jobspresso'
+
+			if tweet_text.include? "Junior"
+				createJob(title, description, url)
+				break
+			end
+
+		elsif website == 'https://twitter.com/nofluffjobs'
+
+			if tweet_text.include? "Junior"
+				if tweet_text.include? "Fully remote job"
+					createJob(title, description, url)
+					break
+				end
+			end
+
+		elsif website == 'https://twitter.com/remote_ok'
+
+			if tweet_text.include? "Junior"
+				createJob(title, description, url)
+				break
+			end
+
+		elsif website == 'https://twitter.com/wfhio'
+
+			if tweet_text.include? "Junior"
+				
+				if tweet_text.include? "[Anywhere]"
+					createJob(title, description, url)
+					break
+				end
+				
+				if tweet_text.include? "[Anywhere*]"
+					createJob(title, description, url)
+					break
+				end
+				
+			end
+
+		end
+
+		i = i + 1
+
+		
 	}
 
+end
+
+
+
+Job.destroy_all
+all_websites = ['https://twitter.com/authenticjobs','https://twitter.com/StackDevJobs', 'https://twitter.com/dribbblejobs', 'https://twitter.com/hasjob', 'https://twitter.com/jobmote', 'https://twitter.com/Jobspresso', 'https://twitter.com/nofluffjobs', 'https://twitter.com/remote_ok', 'https://twitter.com/wfhio' ,'https://twitter.com/workingnomads', 'https://twitter.com/weworkremotely']
+websites=['https://twitter.com/authenticjobs','https://twitter.com/StackDevJobs', 'https://twitter.com/dribbblejobs', 'https://twitter.com/hasjob', 'https://twitter.com/jobmote', 'https://twitter.com/Jobspresso', 'https://twitter.com/nofluffjobs', 'https://twitter.com/remote_ok', 'https://twitter.com/wfhio', 'https://twitter.com/workingnomads', 'https://twitter.com/weworkremotely']
+websites.each{|website|
+	scrape(website)
 }
 
 
